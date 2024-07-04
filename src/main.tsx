0b668d7eac6033b412ec1ca6e8985589f0f28ea2
Sergey Nagornykh <serega1905@ukr.net>
2024-07-04 22:28:37 +0300

    :heavy_plus_sign: update(deps): add MUI material

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App/App.tsx'
 import './styles/index.scss'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
   <React.StrictMode>
     <App />
